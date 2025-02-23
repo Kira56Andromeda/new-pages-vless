3ed7b085e0588791ba331d4638cb75ebeb8bf98a
Kira56Andromeda <128589296+Kira56Andromeda@users.noreply.github.com>
2025-02-23 19:39:06 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'a4c83509-8187-4d51-afb3-2b9d437ea7fe';
 
 const พร็อกซีไอพีs = ['143.198.213.197', '35.219.50.99', '43.218.77.16', 'www.gov.se', 'workers.cloudflare.cyou'];
 
